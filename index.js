8e5abad516cf03ace84ad2b8054d876680fca115
Joram Hanson <hansonjoramelvin17@gmail.com>
2026-05-08 08:38:53 +0000

    Updated subtitle for right panel

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//  PASSWORD VALIDATION
  const passwordInput = document.getElementById("password");
   const form = document.querySelector(".form-area");
   const errorText = document.querySelector(".password-error");
@@ -25,3 +26,53 @@
       errorText.textContent = "";
     }
   });
+
+
+  // USERNAME VALIDATION
+  const usernameInput = document.getElementById("username");
+const usernameError = document.querySelector(".username-error");
+
+
+// USERNAME VALIDATION ONLY
+
+function validateUsername() {
+
+  const usernameValue = usernameInput.value.trim();
+
+  // Check if empty
+
+  if (usernameValue === "") {
+
+    usernameError.textContent =
+      "Please enter a username.";
+
+    return false;
+  }
+
+  // Check if only alphabets
+
+  if (!/^[A-Za-z]+$/.test(usernameValue)) {
+
+    usernameError.textContent =
+      "Username must contain alphabets only.";
+
+    return false;
+  }
+
+  // If valid
+
+  usernameError.textContent = "";
+
+  return true;
+}
+
+form.addEventListener("submit", function (e) {
+
+  const isUsernameValid = validateUsername();
+  const isPasswordValid = validatePassword(passwordInput.value);
+
+  if (!isUsernameValid || !isPasswordValid) {
+    e.preventDefault();
+  }
+
+});
